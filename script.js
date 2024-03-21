89a5f75670906713ff771cd18b00671c136ed194
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-03-21 12:36:31 -0400

    Got the list generator working.

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 let nameList = [];
+const p = document.getElementById('p');
 function nameIn() {
     let name; //Stores
-    name = prompt('Enter one name.')
+    name = prompt('Enter one name.');
     nameList.unshift(name);
     console.log(nameList);
 }
 
-   // Make a container element for the list
-   let listContainer = document.createElement('div');
+function listGen() {
+    // Make a container element for the list
+    let listContainer = document.createElement('div');
 
-   // Make the list
-   let listElement = document.createElement('ul');
+    // Make the list
+    let listElement = document.createElement('ul');
 
-   // Make the list item
-   let listItem = document.createElement('li');
+    // Make the list item
+    let listItem = document.createElement('li');
 
-   // Add it to the page
-   document.body.appendChild(listContainer);
-   listContainer.appendChild(listElement);
+    // Add it to the page
+    //document.body.insertAdjacentElement('beforeend', listContainer);
+    p.append(listContainer);
+    listContainer.appendChild(listElement);
 
-   // Set up a loop that goes through the items in listItems one at a time
-   let numberOfListItems = nameList.length;
+    // Set up a loop that goes through the items in listItems one at a time
+    let numberOfListItems = nameList.length;
 
-   for (let i = 0; i < numberOfListItems; ++i) {
-       // Add the item text
+    for (let i = 0; i < numberOfListItems; ++i) {
+    // Use this if the array elements are text only
+        listItem.textContent = nameList[i];
 
-   // Use this if the array elements contain HTML
-       // listItem.innerHTML = listData[i];
-       // If not, use the line below
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
 
-   // Use this if the array elements are text only
-       listItem.textContent = nameList[i];
-
-       // Add listItem to the listElement
-       listElement.appendChild(listItem);
-
-       // Reset the list item
-       listItem = document.createElement('li');
-   }
+        // Reset the list item
+        listItem = document.createElement('li');
+    }
+}
